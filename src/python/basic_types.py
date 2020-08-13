92baf054cd702c70bf6f93258ff812f5e110604f
JYLeeLYJ <JYLeeLYJ@qq.com>
2020-08-14 02:06:45 +0800

    [add] basic type check functions

--- src/python/basic_types.py
+++ src/python/basic_types.py
@@ -2,7 +2,7 @@ import taichi as ti
 
 Float = ti.f32
 Int = ti.i32
-Bool = ti.i32   # 0 is False , else is True
+# Bool = ti.i32   # 0 is False , else is True
 
 Vector = ti.template()
 Matrix = ti.template()
@@ -12,3 +12,11 @@ Index = Vector
 # ScalarGrid = Grid
 # VectorGrid = Grid
 
+INT_TYPEs = [int , ti.i8 , ti.i16 , ti.i32 , ti.i64]
+FLAOT_TYPEs = [float , ti.f32 , ti.f64]
+
+def is_int_type(dtype):
+    return dtype in INT_TYPEs
+
+def is_float_type(dtype):
+    return dtype in FLAOT_TYPEs
