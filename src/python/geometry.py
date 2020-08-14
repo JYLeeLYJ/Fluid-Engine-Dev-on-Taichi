be796e0e176a30557f38557c0b427d5d8370fc96
JYLeeLYJ <JYLeeLYJ@qq.com>
2020-08-14 16:45:08 +0800

    [add] add geometry module , to support collider handling

--- src/python/geometry.py
+++ src/python/geometry.py
@@ -2,26 +2,91 @@ from abc import ABCMeta , abstractmethod
 from math import cos , sin
 import taichi as ti
 
+from helper_functions import clamp , distance , distance_sqr
+from basic_types import Vector , Float , Int
+
+"""
+TODO: Not well complete . 
+    1. bounding_box query 
+    2. ray intersects check
+
+"""
+
 @ti.data_oriented
 class Ray:
-    def __init__(self , origin  , direction ):
+    def __init__(self , origin : Vector , direction :Vector):
         self.origin = origin
         self.direction = direction
 
-    def point_at( self , t : ti.f32):
-        return origin + direction * t
-        
+    @ti.func
+    def point_at( self , t : Float ):
+        return self.origin + self.direction * t
+
+@ti.data_oriented
+class BoundingBox:
+    def __init__(self , point1 , point2 ):
+    # def __init__(self , lower_corner , upper_corner):
+        self.lower_corner = ti.Vector([min(point1[0] ,point2[0]) , min(point1[1] , point2[1])])
+        self.upper_corner = ti.Vector([max(point1[0] ,point2[0]) , max(point2[1] , point2[1])])
+
+    @ti.func
+    def width(self):
+        return self.upper_corner[0] - self.lower_corner[0]
+
+    @ti.func
+    def height(self):
+        return self.upper_corner[1] - self.lower_corner[1]
+
+    """
+    # def overlaps(self , other) -> bool:
+    #     return not ( \
+    #         (self.upper_corner[0] < other.upper_corner[0] or self.lower_corner[0] > other.lower_corner[0]) \
+    #         or
+    #         (self.upper_corner[1] < other.upper_corner[1] or self.lower_corner[1] > other.upper_corner[1]) \
+    #     )
+    """
+
+    @ti.func
+    def contains(self , point)->bool :
+        return self.upper_corner[0] >= point[0] and self.lower_corner[0] <= point[0] \
+            and self.upper_corner[1] >= point[1] and self.lower_corner[1] <= point[1]      
+
+
+    """
+    # def intersects(self , ray)->bool:
+    #     tmin , tmax = 0 , 1e8
+    #     ray_dir_inv = ti.Vector([1.0/ray.direction[0] , 1.0/ray.direction[1]])
+
+    #     ret = True
+
+    #     for i in ti.static(range(2)):
+    #         near = (self.lower_corner[i] - ray.origin[i]) * ray_dir_inv[i]
+    #         far = (self.upper_corner[i] - ray.origin[i]) * ray_dir_inv[i]
+
+    #         if near > far :
+    #             near ,far = far , near
+            
+    #         tmin = max(near , tmin)
+    #         tmax = min(far , tmax)
+
+    #         if tmin > tmax :
+    #             ret = False
+
+    #     return ret
+    """
+
 @ti.data_oriented
 class Transform:
     def __init__ (self ):
-        pass
+        self._translation = ti.Vector([0.0 ,0.0])
+        self._orientation = 0.0       
 
     @property
     def translation (self):
         return self._translation
 
     @translation.setter
-    def translation(self , translation ):
+    def translation(self , translation : Vector ):
         self._translation = translation
 
     @property
@@ -34,71 +99,292 @@ class Transform:
         self._cos_angle = cos(orientation)
         self._sin_angle = sin(orientation)
 
-    def to_local(self , point_in_world):
+    @ti.func
+    def to_local(self , point_in_world : Vector) -> Vector:
         return self.to_local_direction(point_in_world - self.translation)
     
-    def to_local_direction(self , dir_in_world):
+    @ti.func
+    def to_local_direction(self , dir_in_world : Vector) -> Vector:
         return ti.Vector([
             self._cos_angle * dir_in_world[0] + self._sin_angle * dir_in_world[1] ,
             -self._sin_angle * dir_in_world[0] + self._cos_angle * dir_in_world[1]
         ])
 
-    def to_world(self , point_in_local):
+    @ti.func
+    def to_local_ray(self , ray : Ray) -> Ray:
+        return  Ray(self.to_local(ray.origin) , self.to_local_direction(ray.direction))
+
+    """
+    # def to_local_boundingbox(self , box : BoundingBox) -> BoundingBox:
+    #     pass
+    """
+
+    @ti.func
+    def to_world(self , point_in_local : Vector)->Vector:
         return  self.to_world_direction(point_in_local) + self.translation
 
-    def to_world_direction(self , dir_in_local):
+    @ti.func
+    def to_world_direction(self , dir_in_local : Vector)->Vector:
         return ti.Vector([
-            self._cos_angle * point_in_local[0] - self._sin_angle * point_in_local[1] + self.translation[0] , 
-            self._sin_angle * point_in_local[0] - self._cos_angle * point_in_local[1] + self.translation[1]
+            self._cos_angle * dir_in_local[0] - self._sin_angle * dir_in_local[1] + self.translation[0] , 
+            self._sin_angle * dir_in_local[0] - self._cos_angle * dir_in_local[1] + self.translation[1]
         ])
 
-@ti.data_oriented
-class BoundingBox:
-    def __init__(self , point1 , point2 ):
-        self.lower_corner = [min(point1[0] ,point2[0]) , min(point1[1] , point2[1])]
-        self.upper_corner = [max(point1[0] ,point2[0]) , max(point2[1] , point2[1])]
+    @ti.func
+    def to_world_ray(self, ray : Ray) -> Ray:
+        return Ray(self.to_world(ray.origin) , self.to_world_direction(ray.direction))
 
-    @property
-    def width(self):
-        return self.upper_corner[0] - self.lower_corner[0]
+    """
+    # def to_world_boundingbox(self , box : BoundingBox) -> BoundingBox:
+    #     pass
+    """
 
-    @property
-    def height(self):
-        return self.upper_corner[1] - self.lower_corner[1]
+"""
+# class SurfaceRayIntersection:
+#     def __init__(self):
+#         self.is_intersecting = False
+#         self.distance = 1e8
+#         self.point = [0.0 ,0.0]
+#         self.normal = [0.0 ,0.0]
 
-    def overlaps(self , other) -> bool:
-        return not ( \
-            (self.upper_corner[0] < other.upper_corner[0] or self.lower_corner[0] > other.lower_corner[0]) \
-            or
-            (self.upper_corner[1] < other.upper_corner[1] or self.lower_corner[1] > other.upper_corner[1]) \
-        )
+#     def to_world(self , transform : Transform , is_normal_flipped : bool) :
+#         self.point = transform.to_world(self.point)
+#         self.normal = transform.to_world_direction(self.normal) 
 
-    def contains(self , point)->bool :
-        return self.upper_corner[0] >= point[0] and self.lower_corner[0] <= point[0] \
-            and self.upper_corner[1] >= point[1] and self.lower_corner[1] <= point[1]           
+#         if ti.static(is_normal_flipped is True):
+#             self.normal *= -1,0
+"""
 
 @ti.data_oriented
 class Surface(metaclass = ABCMeta):
 
-    def __init__(self, transfrom , is_normal_flipped):
+    def __init__(self, transfrom : Transform, is_normal_flipped : bool):
         self.transform  = transfrom
         self.is_normal_flipped = is_normal_flipped
 
-    def closestpoint(self , other_point):
-        pass
+    @ti.func
+    def closest_point(self , point : Vector) -> Vector:
+        return self.transform.to_world(self.closest_point_local(self.transform.to_local(point)))
+
+    @ti.func
+    def closest_normal(self , point : Vector ) -> Vector :
+        res = self.transform.to_world_direction(self.closest_normal_local(point))
+        if ti.static(self.is_normal_flipped):
+            res *= -1.0
+        return res
+
+    """
+    # def bounding_box(self):
+    #     pass
+
+    # def intersects(self  , ray : Ray) ->bool:
+    #     return self.intersects_local(self.transform.to_local_ray(ray))
+
+    # def closest_intersection(self , ray : Ray) :
+    #     res = self.closest_interesection_local(self.transform.to_local_ray(ray))
+    #     res.to_world(self.transform , self.is_normal_flipped)
+    #     return res
+    """
+    
+    @ti.func
+    def closest_distance(self , point : Vector)->float:
+        return self.closest_distance_local(self.transform.to_local(point))
+
+    @ti.func
+    def is_inside(self , point : Vector) -> bool :
+        return self.is_normal_flipped is not self.is_inside_local(self.transform.to_local(point))
+
+    ### ====== abstract methods =================================
+    # computations in local space
 
     @abstractmethod
-    def closestnormal(self):
+    def closest_point_local(self , point) -> Vector:
         pass
 
+    """
+    # @abstractmethod
+    # def bounding_box_local(self)-> BoundingBox:
+    #     pass
+
+    # @abstractmethod
+    # def closest_interesection_local(self , ray : Ray):
+    #     pass
+    """
+    
     @abstractmethod
-    def boundingBox(self):
+    def closest_normal_local(self , point) ->Vector:
         pass
 
+    """
+    # def intersects_local(self , ray : Ray) ->bool:
+    #     return self.closest_interesection_local(ray).is_intersecting
+    """
+
+    @ti.func
+    def closest_distance_local(self , point:Vector) -> Float:
+        return distance(point , self.closest_point_local(point))
+
+    @ti.func
+    def is_inside_local(self , point : Vector )->bool :
+        pl = self.closest_point_local(point)
+        nl = self.closest_normal_local(point)
+        return (point - pl).dot(nl) < 0.0
+    
+
+class ImplicitSurface(Surface):
+    def __init__(self , transform : Transform , is_normal_flipped : bool):
+        super().__init__(transform, is_normal_flipped)
+
     @abstractmethod
-    def intersects(self  , ray : Ray)->bool:
+    def sign_distance_local(self ,point : Vector) -> Float:
+        pass
+
+    @ti.func
+    def sign_distance(self , point : Vector) -> Float :
+        dis = self.sign_distance_local(self.transform.to_local(point))
+        return -dis if self.is_normal_flipped else dis
+
+    @ti.func
+    def closest_distance_local(self, point :Vector)->Float:
+        return abs(self.sign_distance_local(point))
+
+    @ti.func
+    def is_inside_local(self ,point: Vector)->bool:
+        return self.sign_distance_local(point) < 0.0
+
+class SurfaceToImplict(ImplicitSurface):
+    def __init__(
+        self , 
+        surface : Surface , 
+        transform : Transform = Transform() , 
+        is_normal_flipped : bool = False):
+
+        super().__init__(transform , is_normal_flipped)
+        self._surface = surface
+
+    def surface(self) -> Surface :
+        return self._surface
+
+    @ti.func
+    def closest_point_local(self , point : Vector)->Vector:
+        return self._surface.closest_point(point)
+
+    @ti.func
+    def closest_distance_local(self , point : Vector)->Float:
+        return self._surface.closest_distance(point)
+
+    """
+    # def intersects_local(self , ray  : Ray) -> bool :
+    #     return self._surface.intersects(ray)
+
+    # def bounding_box_local(self) -> BoundingBox:
+    #     return self._surface.bounding_box()
+    """
+
+    @ti.func
+    def closest_normal_local(self , point : Vector) -> Vector:
+        return self._surface.closest_normal(point)
+
+    @ti.func
+    def sign_distance_local(self , point : Vector) -> Float:
+        p = self._surface.closest_point(point)
+        return - distance(p , point) if self._surface.is_inside(point) else distance(p , point)
+
+    """
+    # def closest_interesection_local(self , ray : Ray) -> SurfaceRayIntersection:
+    #     return self._surface.closest_intersection(ray)
+    """
+    
+    @ti.func
+    def is_inside_local(self , point : Vector) -> bool:
+        return self._surface.is_inside(point)
+
+class Plane(Surface):
+    def __init__(self , normal , point , transfrom = Transform() , is_normal_flipped = False):
+        super().__init__(transfrom , is_normal_flipped)
+
+        self._normal = normal
+        self._point = point
+
+    @ti.func
+    def closest_point_local(self ,point : Vector)->Vector:
+        r = point - self._point
+        return r - self._normal.dot(r) *  self._normal + self._point
+
+    """
+    # def intersects_local(self ,ray : Ray)->bool:
+    #     return abs(ray.direction.dot(self._normal)) > 0
+
+    # def bounding_box_local(self)->BoundingBox:
+    #     pass
+    """
+    
+    @ti.func
+    def closest_normal_local(self , point :Vector)->Vector:
+        return self._normal
+
+    """
+    # def closest_interesection_local(self , ray :Ray)->SurfaceRayIntersection:
+    #     intersect = SurfaceRayIntersection()
+    #     dn = ray.direction.dot(self._normal)
+    #     if abs(dn) > 0:
+    #         t = self._normal.dot(self._point - ray.origin) / dn
+    #         if t >= 0 :
+    #             intersect.is_intersecting = True
+    #             intersect.distance = t
+    #             intersect.point = ray.point_at(t)
+    #             intersect.normal = self._normal
         
+    #     return intersect
+    """
 
-    @abstractmethod
-    def closestDistance(self , point )->float:
-        pass
+class Box(Surface):
+    def __init__(
+        self ,
+        lower_corner , 
+        upper_corner , 
+        transfrom = Transform() , 
+        is_normal_flipped = False ):
+
+        super().__init__(transfrom , is_normal_flipped)
+        self._bound = BoundingBox(lower_corner , upper_corner)
+        self._plane = [
+            Plane(ti.Vector([1,0]), self._bound.upper_corner),
+            Plane(ti.Vector([0,1]), self._bound.upper_corner),
+            Plane(ti.Vector([-1,0]),self._bound.lower_corner),
+            Plane(ti.Vector([0,-1]),self._bound.lower_corner)
+        ]
+
+    @ti.func
+    def closest_point_local(self ,point : Vector)->Vector:
+        result = point
+        dis = 1e8
+        if self._bound.contains(point):
+            for i in ti.static(range(4)):
+                p = self._plane[i].closest_point_local(point)
+                d = distance_sqr(p , point)
+                if dis > d:
+                    dis = d
+                    result = p
+        else:
+            result = clamp(point , self._bound.lower_corner , self._bound.upper_corner)
+
+        return result
+
+    """
+    # def intersects_local(self ,ray : Ray)->bool:
+    #     return self._bound.intersects(ray)
+
+    # def bounding_box_local(self)->BoundingBox:
+    #     pass
+    """
+    
+    @ti.func
+    def closest_normal_local(self , point :Vector)->Vector:
+        #TODO
+        return 
+
+    """
+    # def closest_interesection_local(self , ray :Ray)->SurfaceRayIntersection:
+    #     pass
+    """
